3ea692c5cbed42e90407f76d8df5638ed171d992
elikaye <elianakaye13@gmail.com>
2024-12-13 19:30:34 -0300

    funciones return y console.log diferencias

--- operadoresDeComparacion.js
+++ operadoresDeComparacion.js
@@ -1,4 +1,12 @@
-console.log(4 < 7);
-console.log(4 < 1);
-console.log(4 > 4);
-console.log(4 == 7);
+ //console.log(4 < 7);
+//console.log(4 < 1);
+//console.log(4 > 4);
+//console.log(4 == 7);
+
+
+//operadores de comparacion y igualdad
+
+3 == 3;
+3 === 3;
+3 == '3';
+3 === '3';
